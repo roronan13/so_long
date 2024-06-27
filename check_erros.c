d8cf834c45c6360c5191874e83e73a282bb31ac2
roronan13 <ronan.pothier@gmail.com>
2024-06-27 23:13:15 +0200

    27 juin

--- check_erros.c
+++ check_erros.c
@@ -6,13 +6,24 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 20:20:51 by rpothier          #+#    #+#             */
-/*   Updated: 2024/06/27 20:23:41 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/06/27 23:11:47 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	check_errors(int argc, char **argv)
+int	check_args(int argc)
 {
-	
+	if (argc != 2)
+		return (1);
+}
+
+int	check_extension(char *name, char *ext)
+{
+	int	name_len;
+	int	ext_len;
+
+	name_len = ft_strlen(name);
+	ext_len = ft_strlen(ext);
+	ft_strncmp(&name[name_len - ext_len], ext, ext_len);
 }
