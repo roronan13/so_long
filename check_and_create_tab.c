0854ea10d01ae1a082607d6a0165fdfa023f11a5
roronan13 <ronan.pothier@gmail.com>
2024-07-02 17:14:38 +0200

    2 juil

--- check_and_create_tab.c
+++ check_and_create_tab.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 15:21:14 by rpothier          #+#    #+#             */
-/*   Updated: 2024/07/02 15:53:51 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/07/02 17:14:31 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,14 @@ char	**check_and_create_tab(char **argv)
 	int		line_len;
 	int		fd;
 	char	**map_tab;
+	char	*line;
+	int		i;
+	int		j;
 
 	line_nbr = 1;
+	i = 0;
+	j = 0;
+	
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		exit((ft_printf("Error\nOpening map file failed !\n"), 1));
@@ -27,38 +33,50 @@ char	**check_and_create_tab(char **argv)
 	while (get_next_line(fd))
 		line_nbr++;
 		
-	map_tab = malloc(sizeof(char *) * line_nbr);
+	map_tab = malloc(sizeof(char *) * (line_nbr + 1));
+
+	close(fd);
+	fd = open(argv[1], O_RDONLY);
+	/* if (fd == -1)
+		exit((ft_printf("Error\nOpening map file failed !\n"), 1));
+		// free; */
+	line = get_next_line(fd);
+	while (line)
+	{
+		map_tab[i] = malloc(sizeof(char) * line_len);
+		while (j < line_len - 1)
+		{
+			map_tab[i][j] = line[j];
+			j++;
+		}
+		map_tab[i][j] = '\0';
+		j = 0;
+		i++;
+		free(line);		
+		line = get_next_line(fd);
+	}
+	ft_printf("i : %d\n", i);
+	map_tab[i] = NULL;
 	
 	ft_printf("line_nbr : %d\n", line_nbr);
 	ft_printf("line_len : %d\n", line_len);
+
+	i = 0;
+	j = 0;
+	while (map_tab[i])
+	{
+		while(map_tab[i][j])
+		{
+			ft_printf("%c", map_tab[i][j]);
+			j++;
+		}
+		ft_printf("\n");
+		j = 0;
+		i++;
+	}
 
 	
 	
 	close(fd);
 	return (map_tab);
 }
-/* int line_number(char **argv)
-{
-    int line_nbr;
-    char *line;
-    int fd;
-
-    line_nbr = 0;
-    	fd = open(argv[1], O_RDONLY);
-
-    printf("fd : %d\n", fd);
-    line = get_next_line(fd);
-        // line = get_next_line(fd);
-        
-        
-
-    printf("line : %s\n", line);
-    while (line)
-    {
-        // free(line);
-        line_nbr++;
-        line = get_next_line(fd);
-    }
-    close(fd);
-    return (line_nbr);
-} */
