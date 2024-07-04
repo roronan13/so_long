8a469cfba69438353939e51a08437c8a1d7c211a
roronan13 <ronan.pothier@gmail.com>
2024-07-04 17:21:09 +0200

    4 juil

--- check_and_create_tab.c
+++ check_and_create_tab.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 15:21:14 by rpothier          #+#    #+#             */
-/*   Updated: 2024/07/04 16:28:10 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/07/04 17:17:47 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,10 +62,45 @@ void	check_rectangle(char **map_tab)
 	}
 }
 
-char	**create_tab(char **argv)
+void	check_walls(char **map_tab, int line_nbr)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map_tab[i])
+	{
+		if (i == 0 || i == line_nbr - 1)
+		{
+			j = 0;
+			while (map_tab[i][j] && map_tab[i][j] != '\n')
+			{
+				if (map_tab[i][j] != '1')
+				{
+					ft_free_double(map_tab);
+					ft_printf("Error\nThis map is not surrounded by walls !\n");
+					exit(1);
+				}
+				j++;
+			}
+		}
+		else
+		{
+			if (map_tab[i][0] != '1' || map_tab[i][ft_strlen(map_tab[i]) - 2] != '1')
+			{
+				ft_free_double(map_tab);
+				ft_printf("Error\nThis map is not surrounded by walls !\n");
+				exit(1);
+			}
+		}
+		i++;
+	}
+}
+
+char	**create_tab(char **argv, int line_nbr)
 {
 	char	**map_tab;
-	int		line_nbr;
+	// int		line_nbr;
 	int		i;
 	int		fd;
 
@@ -73,7 +108,7 @@ char	**create_tab(char **argv)
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		exit((perror("Error\nOpening map file failed"), 1));
-	line_nbr = number_of_line(argv, fd);
+	// line_nbr = number_of_line(argv, fd);
 	map_tab = malloc(sizeof(char *) * (line_nbr + 1));
 	if (!map_tab)
 	{
@@ -99,14 +134,17 @@ char	**create_tab(char **argv)
 char	**check_and_create_tab(char **argv)
 {
 	char	**map_tab;
+	int		line_nbr;
 	
-	map_tab = create_tab(argv);
+	line_nbr = number_of_line(argv, -1);
+	map_tab = create_tab(argv, line_nbr);
 	if (!map_tab)
 	{
 		perror("Error\nMalloc failed");
 		exit(errno);
 	}
 	check_rectangle(map_tab);
+	check_walls(map_tab, line_nbr);
 	
 
 	return (map_tab);
