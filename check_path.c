4bbe9d951ab051260f0234c2d8e8cfab7b0bf7bd
roronan13 <ronan.pothier@gmail.com>
2024-07-05 20:33:18 +0200

    5 juil

--- check_path.c
+++ check_path.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 18:26:31 by rpothier          #+#    #+#             */
-/*   Updated: 2024/07/05 20:19:46 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/07/05 20:29:16 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ int	check_path(char **map_tab)
 
 	find_player(map_tab, &x, &y);
 	ft_printf("x : %d\ny : %d\n", x, y);
-	flood_fill(map_tab, x , y);
-
+	
 	int i = 0;
 	while (map_tab[i])
 	{
@@ -29,16 +28,19 @@ int	check_path(char **map_tab)
 	}
 	ft_printf("\n%s\n", map_tab[i]);
 	
+	flood_fill(map_tab, x , y);
+
+	
 	return (0);
 }
 
 int	flood_fill(char **map_tab, int x, int y)
 {
-	if (map_tab[x][y] == '1' || map_tab[x][y] == 'F')
+	if (map_tab[y][x] == '1' || map_tab[y][x] == 'F')
 		return (0);
 	
-	map_tab[x][y] = 'F';
-	
+	map_tab[y][x] = 'F';
+	// ft_printf("allo\n");
 	if (flood_fill(map_tab, x - 1, y) || flood_fill(map_tab, x, y - 1) || flood_fill(map_tab, x + 1, y) || flood_fill(map_tab, x, y + 1))
 	{
 		return (1);
